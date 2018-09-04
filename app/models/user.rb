9a79f78d347bdcfd0f58cf71740bc5ff2b34e87e
Andrew <thesimpledev@gmail.com>
2018-09-04 03:39:25 -0700

    fix logic error. Was rehashing passwords on save even if it was already hashed

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,14 @@ class User < ApplicationRecord
   private
 
   def hash_password
-    self.password = Password.create(password)
+    self.password = Password.create(password) unless password_already_hashed?
   end
 
   def unhashed_password
     Password.new(password)
+  end
+
+  def password_already_hashed?
+    Password.valid_hash?(password)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe User, type: :model do
     expect(BCrypt::Password.valid_hash?(user.password)).to be_truthy
   end
 
+  it "doesn't rehash password if already hashed" do
+    password_hash_before = user.password
+    user.save
+    user.reload
+
+    expect(user.password).to eq(password_hash_before)
+  end
+
   describe "#authenticate" do
     context "when password correct" do
       it "returns true" do
