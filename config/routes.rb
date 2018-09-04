7e58b09aa9e139a0881c15dff049d73709eb8032
Andrew <thesimpledev@gmail.com>
2018-09-04 14:56:04 -0700

    add optional email parameter to get_started & login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   resources :users, except: %i[new create]
-  get "get-started" => "users#new"
+  get "get-started(/:email)" => "users#new", as: "get_started"
   post "get-started" => "users#create"
 
   root "pages#landing"
   get "dashboard" => "pages#dashboard"
   get "admin" => "pages#admin"
 
-  get "login" => "sessions#new"
+  get "login(/:email)" => "sessions#new", as: "login"
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
 
