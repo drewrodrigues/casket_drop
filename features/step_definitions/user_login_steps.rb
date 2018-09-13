cf54903c6a3599e15b7d8273602a4fbc239274d3
Andrew <thesimpledev@gmail.com>
2018-09-13 15:08:06 -0700

    add user login acceptance tests

---
+++ features/step_definitions/user_login_steps.rb
@@ -0,0 +1,28 @@
+Given /^I am a registered user$/ do
+  @user = FactoryBot.create(:user, password: "password")
+end
+
+When /^I am on the login page$/ do
+  visit login_path
+end
+
+When /^I fill out the form with my email and password$/ do
+  fill_in "Email", with: @user.email
+  fill_in "Password", with: "password"
+end
+
+When /^I click login$/ do
+  click_button "Login"
+end
+
+Then /^I should see the dashboard page$/ do
+  expect(page).to have_content("Welcome to your dashboard, #{@user.first_name}!") 
+end
+
+Then /^I should see the login page$/ do
+  expect(page).to have_button("Login", count: 1)
+end
+
+Then /^I should see a login error$/ do
+  expect(page).to have_content("There was a problem with your login.")
+end
---
+++ features/user_login.feature
@@ -0,0 +1,19 @@
+Feature: User login
+  As a user
+  I want to be able to login
+  So that I can view my account information
+
+  Scenario: User logs in
+    Given I am a registered user
+    Given I am on the login page
+    When I fill out the form with my email and password
+    And I click login
+    Then I should see the dashboard page
+
+  Scenario: User fails to login
+    Given I am on the login page
+    When I click login
+    Then I should see the login page 
+    And I should see a login error 
+
+
