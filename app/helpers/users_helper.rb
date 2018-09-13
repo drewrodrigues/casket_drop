9c81dd26bf9fbf85311f5b774cbd62a4de81afe5
Andrew <thesimpledev@gmail.com>
2018-09-13 15:06:49 -0700

    add default & disabled select option

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,21 +1,13 @@
 module UsersHelper
   def bottom_fit_options
-    %w(Skinny Straight Relaxed)
-  end
-
-  def shoe_size_options
-    [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5]
-  end
-
-  def style_options
-    %w(Athletic Casual Formal Hipster Street)
+    ["Select your bottom fit", "Relaxed", "Skinny", "Straight"]
   end
 
   def top_size_options
-    %w(SM MD LG XL)
+    ["Select your top size", "SM", "MD", "LG", "XL"]
   end
 
   def waist_size_options
-    (28..40).to_a
+    ["Select your waist size"] + (28..40).to_a
   end
 end
