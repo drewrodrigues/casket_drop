cd710cc37497d6ff8f8de10143cf196fb925af94
Andrew <thesimpledev@gmail.com>
2018-09-13 15:20:18 -0700

    remove style & shoe_size from user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,9 +5,7 @@ FactoryBot.define do
     city "New York"
     sequence(:email) { |i| "andrew#{i}@example.com" }
     password "password"
-    shoe_size 8
     state "NY"
-    style "Athletic"
     top_size "SM"
     waist_size 30
   end
